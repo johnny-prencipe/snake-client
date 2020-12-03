41ce79f466ee0ba8cf7efd8a558086ce0c82e275
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-03 16:41:19 +0000

    moved connect function to its own file

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require('net');
+//establishes connection w/ game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('data', data => {
+    console.log('Message from server: ', data);
+  });
+
+  return conn;
+}
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,18 +1,3 @@
-const net = require('net');
-//establishes connection w/ game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('data', data => {
-    console.log('Message from server: ', data);
-  });
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('connecting...');
 connect();
